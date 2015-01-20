77da651a857557029f501f7b415574648e1a24ca
mhcabral <mh.cabral1@gmail.com>
2015-01-19 20:53:16 -0400

    Model TccTcc alterado

--- src/src/java/br/edu/ufam/icomp/tcc/model/TccTcc.java
+++ src/src/java/br/edu/ufam/icomp/tcc/model/TccTcc.java
@@ -9,6 +9,7 @@ import br.edu.ufam.icomp.projeto4.model.Aluno;
 import br.edu.ufam.icomp.projeto4.model.PeriodoLetivo;
 import br.edu.ufam.icomp.projeto4.model.Professor;
 import java.io.Serializable;
+import java.util.List;
 import javax.persistence.*;
 import javax.validation.constraints.*;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -16,7 +17,10 @@ import javax.xml.bind.annotation.XmlRootElement;
 @Entity
 @Table(name = "tcctcc")
 @XmlRootElement
-
+@NamedQueries({
+    @NamedQuery(name = "TccTcc.findByAluno", query = "SELECT t FROM TccTcc t WHERE t.aluno.id = :idAluno"),
+    @NamedQuery(name = "TccTcc.findByProfessor", query = "SELECT t.aluno FROM TccTcc t WHERE t.professor.id = :idProfessor ORDER BY t.aluno.usuario.nome")
+})
 public class TccTcc implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
@@ -36,23 +40,21 @@ public class TccTcc implements Serializable {
     @Size(min = 1, max = 1024)
     @Column(name = "descricao")
     private String descricao;
-    @Basic(optional = false)
-    @NotNull
-    @Size(min = 1, max = 20)
-    @Column(name = "estado")
+    @Transient
     private String estado;
-    @ManyToOne
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "id_aluno")
     private Aluno aluno;
-    @ManyToOne
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "id_professor")
     private Professor professor;
-    @ManyToOne
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "id_periodo")
     private PeriodoLetivo periodo;
-    @ManyToOne
-    @JoinColumn(name = "id_tcctema")
-    private TccTema tcctema;
+    @OneToOne(mappedBy="tccTcc")
+    private TccSolicitacao solicitacaoTema;
+    @ElementCollection(targetClass = String.class)
+    private List<String> anexos;
 
     public Long getId() {
         return id;
@@ -79,11 +81,16 @@ public class TccTcc implements Serializable {
     }
 
     public String getEstado() {
+        if (this.solicitacaoTema == null) {
+            estado = "Aberto";
+        } else if (this.solicitacaoTema.getEstado().equals("Solicitado")) {
+            estado = "Solicitando Tema";
+        } else if (this.solicitacaoTema.getEstado().equals("Deferido")) {
+            estado = "Tema Deferido";
+        } else if (this.solicitacaoTema.getEstado().equals("Indeferido")) {
+            estado = "Tema Indeferido";
+        }
         return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
     }
 
     public Aluno getAluno() {
@@ -110,14 +117,23 @@ public class TccTcc implements Serializable {
         this.periodo = periodo;
     }
 
-    public TccTema getTcctema() {
-        return tcctema;
+    public TccSolicitacao getSolicitacaoTema() {
+        return solicitacaoTema;
     }
 
-    public void setTcctema(TccTema tcctema) {
-        this.tcctema = tcctema;
+    public void setSolicitacaoTema(TccSolicitacao solicitacaoTema) {
+        this.solicitacaoTema = solicitacaoTema;
     }
 
+    public List<String> getAnexos() {
+        return anexos;
+    }
+
+    public void setAnexos(List<String> anexos) {
+        this.anexos = anexos;
+    }
+    
+    
     @Override
     public String toString() {
         return titulo;
